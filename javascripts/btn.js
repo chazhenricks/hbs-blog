8f454b7ced89081d896b6b681eb1727eebe224de
Chaz Henricks <chazhenricks@gmail.com>
2017-05-28 12:18:09 -0500

    basic components added

--- javascripts/btn.js
+++
@@ -1,8 +0,0 @@
-var enterButton = document.getElementById("enter");
-
-
-enterButton.addEventListener("click", navigate);
-
-function navigate(){
-  window.location.href = "blog.html";
-}
---
+++ js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+console.log("Main.js");
+
+
+
+
+
+// var container = document.getElementById("container");
+// var links = document.getElementById("list-group");
+
+// for (var i=0;i<blogPosts.length;i++){
+//   var posts =
+//     `<article class ="container-fluid">
+//       <header class="page-header">
+//         <h3 class="">${blogPosts[i].date}</h3>
+//         <h2 id ="post${i}">${blogPosts[i].title} </h2>
+//       </header>
+//       <section class="">
+//         ${blogPosts[i].copy}
+//       </section>
+//       <footer class="">
+//         <p class="">Written By - ${blogPosts[i].author}
+//       </footer>
+//     </article>`;
+
+//   var linkContent =
+//   `<a href="#post${i}" class="list-group-item">
+//     <h4 class ="list-group-item-heading">${blogPosts[i].title}</h4>
+//     <p class ="list-group-item-text">${blogPosts[i].author}</p>
+//    </a>
+//   `
+
+// container.innerHTML += posts;
+// links.innerHTML += linkContent;
+
+// };
+
+
+
+
+
+
+
+
